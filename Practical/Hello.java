b8edefeb6e91c5761706082cffc0a6672b49df70
Sheetal Ambre <104828396+Sheetal002@users.noreply.github.com>
2023-01-20 18:04:52 +0530

    Add files via upload

--- Practical/Hello.java
+++ Practical/Hello.java
@@ -1,5 +1,13 @@
-class Hello{
-	public static void main(String args[]){
-		System.out.println("Printing a Program");
-	}
+class My{
+
+	public static void display() {//static method
+		System.out.println("Hi ....");
+		}
 }
+class Hello{//start of the class
+	public static void main(String args[]){ //entry point
+		System.out.println("Its my first program.");
+		
+		My.display(); //classname.methodname
+	}
+}//end of the class
