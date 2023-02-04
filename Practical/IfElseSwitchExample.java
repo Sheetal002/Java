e3e33eecfcf20b94375974b3ef0d658a1c7e4da1
Sheetal Ambre <104828396+Sheetal002@users.noreply.github.com>
2023-02-04 09:46:30 +0530

    Add files via upload

---
+++ Practical/IfElseSwitchExample.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class IfElseSwitchExample{
+	
+	public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age: ");
+        int age = sc.nextInt();
+        
+        
+		
+
+		if (age >=40 && age <=65){
+        System.out.println("Enter shift: ");
+        String shift = sc.next();            
+			
+			switch(shift){
+				
+				case "First" :
+				System.out.println("first shift allowed 6am - 3pm");
+				break;
+				
+				case "Second" :
+				System.out.println("Second shift allowed 4pm - 10 pm");
+				break;
+			}
+			
+		}
+		else if(age >=20 && age <40){
+            System.out.println("Enter shift: ");
+        String shift1 = sc.next();
+		    
+			switch(shift1){
+				
+				case "Night" :
+				System.out.println("Night shift allowed 11pm - 5 am");
+				break;
+				
+				case "General" :
+				System.out.println("General shift allowed 10am - 6 pm");
+				break;
+				
+			}
+			
+		}
+		else{
+			System.out.println("Age is not proper");
+		}
+		
+	}
+}
