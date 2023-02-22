7dfe0efb783eda2329e76c3cf96d5bcd8c81b273
Sheetal Ambre <104828396+Sheetal002@users.noreply.github.com>
2023-02-22 14:23:57 +0530

    Add files via upload

---
+++ Assignment/EvenOddPosition.java
@@ -0,0 +1,16 @@
+public class EvenOddPosition {
+
+	public static void main(String[] args) {
+		int [] arr= new int []{1,2,3,4,5,6};
+		System.out.println("Odd Position: ");
+		for (int i=0;i<arr.length;i=i+2) {
+			System.out.println(arr[i]);
+		}
+		System.out.println("Even position: ");
+		for (int j = 1; j < arr.length; j = j+2) {
+			System.out.println(arr[j]);
+		}
+
+	}
+
+}
